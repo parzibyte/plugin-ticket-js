7c3af51cea5c5a0082d3920a93be4ec6a637b0be
Luis Cabrera Benito <parzibyte@gmail.com>
2019-11-21 19:57:38 -0600

    Ejemplo de impresión de QR

--- ejemplos/qr/script.js
+++ ejemplos/qr/script.js
@@ -1,6 +1,55 @@
-let impresora = new Impresora();
-impresora.write("Probando codigos QR desde JS!");
-impresora.qr("Proudly brought to you by Parzibyte");
-impresora.write("Podemos tener un poco de texto...");
-impresora.qr("https://parzibyte.me");
-impresora.end();
+const $estado = document.querySelector("#estado"),
+    $listaDeImpresoras = document.querySelector("#listaDeImpresoras"),
+    $codigo = document.querySelector("#codigo"),
+    $btnLimpiarLog = document.querySelector("#btnLimpiarLog"),
+    $btnImprimir = document.querySelector("#btnImprimir");
+const loguear = texto => $estado.textContent += (new Date()).toLocaleString() + " " + texto + "\n";
+const limpiarLog = () => $estado.textContent = "";
+
+$btnLimpiarLog.addEventListener("click", limpiarLog);
+const limpiarLista = () => {
+    for (let i = $listaDeImpresoras.options.length; i >= 0; i--) {
+        $listaDeImpresoras.remove(i);
+    }
+};
+const obtenerListaDeImpresoras = () => {
+    loguear("Cargando lista...");
+    Impresora.getImpresoras()
+        .then(listaDeImpresoras => {
+            loguear("Lista cargada");
+            limpiarLista();
+            listaDeImpresoras.forEach(nombreImpresora => {
+                const option = document.createElement('option');
+                option.value = option.text = nombreImpresora;
+                $listaDeImpresoras.appendChild(option);
+            })
+        });
+};
+obtenerListaDeImpresoras();
+/*
+* La magia sucede a continuación
+* */
+$btnImprimir.addEventListener("click", () => {
+    // Recuperar el QR
+    let contenido = $codigo.value;
+    // Validar
+    if (!contenido) {
+        return alert("Escribe el contenido del QR");
+    }
+    // Ahora la impresora
+    let impresoraSeleccionada = $listaDeImpresoras.options[$listaDeImpresoras.selectedIndex].value;
+    if (!impresoraSeleccionada) {
+        return alert("Selecciona una impresora")
+    }
+    // Si está bien se ejecuta esto...
+    let impresora = new Impresora();
+    impresora.qr(contenido);
+    // Dos saltos de línea
+    impresora.feed(2);
+    // Terminar en la impresora seleccionada
+    loguear("Imprimiendo...");
+    impresora.imprimirEnImpresora(impresoraSeleccionada)
+        .then(respuesta => {
+            loguear("Al imprimir el código QR tenemos: " + respuesta);
+        })
+});
